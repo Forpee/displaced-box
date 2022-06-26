3d76b531e79c380841c009e8c794e5fbfba340a1
Forpee <khalilhassam3@gmail.com>
2022-06-26 20:08:09 +0200

    a

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -7,6 +7,16 @@ varying vec2 vUv;
 
 void main()
 {
+    
+    vec4 white=vec4(1.,1.,1.,1.);
+    vec4 red=vec4(1.,.16,.32,1.);
+    vec4 blue=vec4(.16,0.,0.,1.);
+    
+    vec4 finalColor=mix(blue,red,smoothstep(0.,.7,vHeight));
+    
+    finalColor=mix(finalColor,white,smoothstep(.7,.9,vHeight));
+    
     vec4 color1=texture2D(uTexture1,vUv);
     gl_FragColor=vec4(vHeight,0.,0.,1.);
+    gl_FragColor=finalColor;
 }
