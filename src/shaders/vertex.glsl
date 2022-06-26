1f422f2e9f67fc04d81c351a56c08a4da0be7cd3
Forpee <khalilhassam3@gmail.com>
2022-06-26 20:01:24 +0200

    a

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,4 +1,5 @@
 varying vec2 vUv;
+varying float vHeight;
 uniform float uTime;
 uniform sampler2D uTexture1;
 uniform sampler2D uTexture2;
@@ -9,13 +10,23 @@ void main()
     // gl_PointSize=100.*(1./-mvPosition.z);
     
     vec3 newpos=position;
-    vUv=position.xz+vec2(.5,.5);
+    vUv=uv;
+    vec2 uv1=position.xz+vec2(.5,.5);
     
-    vec2 vUv1=uv+uTime*.05;
+    vec2 vUv1=uv1+uTime*.05;
+    vec2 vUv2=uv1-uTime*.05;
+    
+    vUv1*=.2;
+    vUv2*=.5;
+    
+    float top=step(0.,position.y);
     
     vec4 bump1=texture2D(uTexture1,vUv1);
+    vec4 bump2=texture2D(uTexture2,vUv2);
     
-    newpos.y+=bump1.x/5.;
+    float height=(bump1.x+bump2.x)/2.;
+    vHeight=height;
+    newpos.y+=top*height/2.5;
     
     gl_Position=projectionMatrix*modelViewMatrix*vec4(newpos,1.);
     
