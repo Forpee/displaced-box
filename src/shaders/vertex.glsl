5db21857ec8d7c842d18295b97e040ca4c837675
Forpee <khalilhassam3@gmail.com>
2022-06-26 19:47:32 +0200

    a

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,10 +1,13 @@
 varying vec2 vUv;
+uniform float uTime;
 
 void main()
 {
     // vec4 mvPosition=modelViewMatrix*vec4(position,1.);
     // gl_PointSize=100.*(1./-mvPosition.z);
-
+    
+    vec3 newpos=position;
+    
     gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1.);
     
     vUv=uv;
